7b3c2ca849fb6eb9e7b0e1cc5ccb043696bfee70
Arthur Pompeu <arthurgpompeu1@gmail.com>
2022-05-19 22:51:31 -0300

    homepage mel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  #before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:home]
   before_action :configure_permitted_parameters, if: :devise_controller?
   #test
   def after_sign_in_path_for(resources)
